53af67d7c99571216be804bc21950966adbfec68
ricekab <contact@kevinchiyantang.com>
2023-11-04 23:16:20 +0100

    Added basic music and sound support. Music is hard-coded one song for now. Sound uses a pool of audiostreamplayers to play a number of sounds.

---
+++ bricksclone/common/audio_sound_pool.gd
@@ -0,0 +1,37 @@
+extends Node
+## Manages a pool of AudioStreamPlayer instances to play sound with.
+
+@export var initial_num_of_streams: int = 1
+var _audio_stream_pool: Array[AudioStreamPlayer] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for n in initial_num_of_streams:
+		_create_audio_stream_player()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# TODO: Periodically clean unused players?
+	pass
+
+func play_sound(sound_resource: AudioStream):
+	var player = _get_audio_stream_player()
+	player.stream = sound_resource
+	player.play()
+
+
+## Get an available AudioStreamPlayer to play a sound on. 
+## If one does not exist, a new one is created.
+func _get_audio_stream_player() -> AudioStreamPlayer:
+	for player in _audio_stream_pool:
+		if not player.playing:
+			return player
+	return _create_audio_stream_player()
+
+func _create_audio_stream_player() -> AudioStreamPlayer:
+	print('creating player ', _audio_stream_pool.size())
+	var new_player = AudioStreamPlayer.new()
+	_audio_stream_pool.append(new_player)
+	add_child(new_player)
+	return new_player
--- bricksclone/common/root.gd
+++ bricksclone/common/root.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@onready var audio_sound_pool = $audio_sound_pool
+var generic_sound = preload("res://sounds/hit_generic.wav")
+
+var elapsed: float = 0.0
+var plays : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +13,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	elapsed += delta
+	if elapsed > 5.0:
+		print("Play sound now ", plays)
+		audio_sound_pool.play_sound(generic_sound)
+		plays += 1
+		if plays > 3:
+			elapsed = 0.0
